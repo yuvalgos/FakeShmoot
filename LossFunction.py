2420b1caf4d1e570fb6da3a316a8d0dcfa09132f
yuvalgos <yuvalgos@gmail.com>
2022-03-04 17:33:13 +0200

    Some changes in loss weighting

--- LossFunction.py
+++ LossFunction.py
@@ -1,10 +1,83 @@
 import torch
 import torch.nn.functional as F
+import torchvision
 
 
 def vae_loss(x, x_reconstructed, z_mean, z_log_var, beta=1.0):
+    """ Original VAE loss function  composed of reconstruction loss and KL divergence """
     recon_loss = torch.sum((x - x_reconstructed) ** 2) / x.numel()
     latent_loss = -0.5 * torch.sum(1 + z_log_var - z_mean.pow(2) - z_log_var.exp()) / x.numel()
 
     loss = recon_loss + beta * latent_loss
     return loss, recon_loss, latent_loss
+
+
+class VGGPerceptualLoss(torch.nn.Module):
+    """ Copied entirely from https://gist.github.com/alper111/8233cdb0414b4cb5853f2f730ab95a49 """
+    def __init__(self, resize=True):
+        super(VGGPerceptualLoss, self).__init__()
+        blocks = []
+        blocks.append(torchvision.models.vgg16(pretrained=True).features[:4].eval())
+        blocks.append(torchvision.models.vgg16(pretrained=True).features[4:9].eval())
+        blocks.append(torchvision.models.vgg16(pretrained=True).features[9:16].eval())
+        blocks.append(torchvision.models.vgg16(pretrained=True).features[16:23].eval())
+        for bl in blocks:
+            for p in bl.parameters():
+                p.requires_grad = False
+        self.blocks = torch.nn.ModuleList(blocks)
+        self.transform = torch.nn.functional.interpolate
+        self.resize = resize
+        self.register_buffer("mean", torch.tensor([0.485, 0.456, 0.406]).view(1, 3, 1, 1))
+        self.register_buffer("std", torch.tensor([0.229, 0.224, 0.225]).view(1, 3, 1, 1))
+
+    def forward(self, input, target, feature_layers=[0, 1, 2, 3], style_layers=[]):
+        if input.shape[1] != 3:
+            input = input.repeat(1, 3, 1, 1)
+            target = target.repeat(1, 3, 1, 1)
+        input = (input-self.mean) / self.std
+        target = (target-self.mean) / self.std
+        if self.resize:
+            input = self.transform(input, mode='bilinear', size=(224, 224), align_corners=False)
+            target = self.transform(target, mode='bilinear', size=(224, 224), align_corners=False)
+        loss = 0.0
+        x = input
+        y = target
+        for i, block in enumerate(self.blocks):
+            x = block(x)
+            y = block(y)
+            if i in feature_layers:
+                loss += torch.nn.functional.l1_loss(x, y)
+            if i in style_layers:
+                act_x = x.reshape(x.shape[0], x.shape[1], -1)
+                act_y = y.reshape(y.shape[0], y.shape[1], -1)
+                gram_x = act_x @ act_x.permute(0, 2, 1)
+                gram_y = act_y @ act_y.permute(0, 2, 1)
+                loss += torch.nn.functional.l1_loss(gram_x, gram_y)
+        return loss
+
+
+class VAEComposedLoss(torch.nn.Module):
+    def __init__(self, kl_beta=1.5, vgg_weight=0.00, VGG_feature_layers=[0, 1, 2, 3],
+                 VGG_style_layers=[], device='auto'):
+        super(VAEComposedLoss, self).__init__()
+
+        if device == 'auto':
+            self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        else:
+            self.device = device
+
+        self.kl_beta = kl_beta
+        self.vgg_weight = vgg_weight
+        self.feature_layers = VGG_feature_layers
+        self.style_layers = VGG_style_layers
+        self.vgg_loss = VGGPerceptualLoss(resize=False,).to(self.device)
+
+    def forward(self, x, x_reconstructed, z_mean, z_log_var):
+        _, recon_mse_loss, latent_loss = vae_loss(x, x_reconstructed, z_mean, z_log_var, beta=self.kl_beta)
+        vgg_loss = self.vgg_loss(x, x_reconstructed, feature_layers=self.feature_layers,
+                                 style_layers=self.style_layers)
+
+        recon_loss = recon_mse_loss + self.vgg_weight * vgg_loss
+        loss = recon_loss + self.kl_beta * latent_loss
+        return loss, vgg_loss, recon_mse_loss, latent_loss
+
--- Train.py
+++ Train.py
@@ -1,5 +1,5 @@
 import torch
-from LossFunction import vae_loss
+from LossFunction import vae_loss, VAEComposedLoss
 from DataSets import ShmootDataSet128
 from DataLoaders import get_shmoot_dataloader
 from VAEModel import Vae
@@ -8,18 +8,21 @@ from torch.utils.tensorboard import SummaryWriter
 import time
 
 
-TB_RUN_NAME = 'MSE'
+TB_RUN_NAME = 'VGG-medium'
 # --- hyper parameters ---
 BATCH_SIZE = 64
-EPOCHS = 5000
+EPOCHS = 7500
 LEARNING_RATE = 1e-4
 LR_DECAY_GAMMA = 0.2
-LATENT_SIZE = 128
-RESIDUAL_BLOCKS = 1
-FC_LAYERS = [256]
-BETA = 1
+LATENT_SIZE = 256
+RESIDUAL_BLOCKS = 4
+FC_LAYERS = [512, 256]
+BETA = 1.5
+VGG_WEIGHT = 0.005
 
-EVAL_FREQ = 50
+NUM_WORKERS = 4
+EVAL_FREQ = 200
+CHECKPOINT_FREQ = 1500
 
 
 def main():
@@ -28,15 +31,16 @@ def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("running on device:", device)
 
-    data_set = ShmootDataSet128(augment=False)
-    data_loader = get_shmoot_dataloader(data_set, batch_size=BATCH_SIZE)
-    model = Vae(latent_size=LATENT_SIZE,
-                num_residual_convs=RESIDUAL_BLOCKS,
-                fc_layers=FC_LAYERS,
-                device=device).to(device)
-    # model = torch.load('')
+    data_set = ShmootDataSet128(augment=True)
+    data_loader = get_shmoot_dataloader(data_set, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS)
+    # model = Vae(latent_size=LATENT_SIZE,
+    #             num_residual_convs=RESIDUAL_BLOCKS,
+    #             fc_layers=FC_LAYERS,
+    #             device=device).to(device)
+    model = torch.load('checkpoints/withVGG7500.pt')
     optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=2*EPOCHS//3, gamma=LR_DECAY_GAMMA)
+    loss_fn = VAEComposedLoss(kl_beta=BETA, vgg_weight=VGG_WEIGHT, device=device)
 
     writer = SummaryWriter(comment=TB_RUN_NAME)
 
@@ -46,7 +50,7 @@ def main():
             im_batch = im_batch.to(device)
             recon_batch, z_mu, z_sigma, z = model(im_batch)
 
-            loss, recon_error, latent_kl = vae_loss(im_batch, recon_batch, z_mu, z_sigma, beta=BETA)
+            loss, vgg_loss, recon_mse_loss, latent_kl = loss_fn(im_batch, recon_batch, z_mu, z_sigma)
 
             optimizer.zero_grad()
             loss.backward()
@@ -54,13 +58,14 @@ def main():
 
             iteration = epoch * len(data_loader) + batch_idx
             writer.add_scalar("Loss/0loss", loss, iteration)
-            writer.add_scalar("Loss/recon_error", recon_error, iteration)
+            writer.add_scalar("Loss/recon_error", recon_mse_loss, iteration)
+            writer.add_scalar("Loss/VGG_loss", vgg_loss, iteration)
             writer.add_scalar("Loss/latent_kl", latent_kl, iteration)
 
         print("Epoch: {}/{}".format(epoch, EPOCHS), "Time: {:.2f}".format(time.time() - start))
+        if epoch % CHECKPOINT_FREQ == 0:
+            torch.save(model, "checkpoints/withVGG{}.pt".format(epoch))
         if epoch % EVAL_FREQ == 0:
-            torch.save(model, "checkpoints/desubresid_{}.pt".format(epoch))
-
             writer.add_image("Images/original_subset", make_image_grid(im_batch[0:8]), epoch)
             writer.add_image("Images/reconstructed_subset", make_image_grid(recon_batch[0:8]), epoch)
             writer.add_image("Images/sampled_images", make_image_grid(model.sample(8)), epoch)
