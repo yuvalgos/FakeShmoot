994a14f754b75417b4f887e68de0c31cc1def043
yuvalgos <yuvalgos@gmail.com>
2022-03-04 19:36:26 +0200

    a little bit more random crop

--- DataSets.py
+++ DataSets.py
@@ -26,7 +26,7 @@ class ShmootDataSet(Dataset):
         if augmentation_transform is None:
             self.augmentation_transform = transforms.Compose([
                 transforms.RandomPerspective(distortion_scale=0.2, p=0.75),
-                transforms.RandomResizedCrop(size=self.im_size, scale=(0.8, 1.0), ratio=(1, 1)),
+                transforms.RandomResizedCrop(size=self.im_size, scale=(0.7, 1.0), ratio=(1, 1)),
                 transforms.RandomHorizontalFlip(p=0.5)
             ])
         else:
